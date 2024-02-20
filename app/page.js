9231c31ed0932c5cb0ca59c07bf60b7c9d43f289
1177345 <dddrag734@gmail.com>
2024-02-20 12:52:22 +0300

    Дз 3 (Авторизация+промокод)
    хуки и жизненный цикл компонента

--- app/page.js
+++ app/page.js
@@ -1,19 +1,21 @@
-// импортируем всё что нам нужно
-import {Banner} from "./Components/Banner/Banner"
-import {Promo} from "./Components/Promo/Promo";
-import { CardsList } from "./Components/CardsList/CardsList";
+// импортируем компоненты
+import { Banner } from './Components/Banner/Banner';
+import { Promo } from './Components/Promo/Promo';
+import { CardsList } from './Components/CardsList/CardsList';
+// импортируем стили
 import Styles from './page.module.css';
-import {getGamesByCategory} from './data/data-utils';
+// импортируем данные
+import { getGamesByCategory } from './data/data-utils';
 
 export default function Home() {
-  const popularGames = getGamesByCategory('popular');
-	const newGames = getGamesByCategory('new'); 
-  return (
-        <main className={Styles["main"]}>
-          <Banner/>
-          <CardsList id="popular" title="Популярные" data = {popularGames}/>
-          <CardsList id="new" title="Новые" data = {newGames} />
-          <Promo/>
-        </main>
-  );
-};
+	const popularGames = getGamesByCategory('popular')
+	const newGames = getGamesByCategory('new')
+	return (
+		<main className={Styles['main']}>
+			<Banner />
+			<CardsList id='popular' title='Популярные' data={popularGames} />
+			<CardsList id='new' title='Новые' data={newGames} />
+			<Promo />
+		</main>
+	)
+}
